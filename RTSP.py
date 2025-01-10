1de277289e311cfcfd4a8659df7e5a154cdd5100
ivank <ivan.karpenko.2004@mail.ru>
2025-01-10 13:25:18 +0300

    fps and fullscrean

--- RTSP.py
+++ RTSP.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 
 def main():
     rtsp_url = "rtsp://root:root@192.168.0.90/axis-media/media.amp"
@@ -8,15 +9,40 @@ def main():
         print("Не удалось открыть RTSP")
         return
 
+    cv2.namedWindow("Axis RTSP Stream", cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty("Axis RTSP Stream", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+    prev_time = time.time()
+    frame_count = 0
+    fps = 0.0
+
     while True:
         ret, frame = cap.read()
         if not ret:
-            print("Ошибка чтения")
+            print("Ошибка чтения кадра.")
             break
+
+        frame_count += 1
+        current_time = time.time()
+        elapsed_time = current_time - prev_time
+        if elapsed_time >= 1.0:
+            fps = frame_count / elapsed_time
+            frame_count = 0
+            prev_time = current_time
+
+        cv2.putText(
+            frame, 
+            f"FPS: {fps:.2f}", 
+            (10, 30), 
+            cv2.FONT_HERSHEY_SIMPLEX, 
+            1.0, 
+            (0, 255, 0), 
+            2
+        )
 
         cv2.imshow("Axis RTSP Stream", frame)
 
-        #  'q', чтобы выйти из цикла
+        # Нажмите 'q', чтобы выйти
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -25,4 +51,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-#pip install opencv-python
